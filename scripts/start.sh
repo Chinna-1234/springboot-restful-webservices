1b151ddf89f8347cc8d71a5d78d10c52bbe487f4
Chinna-1234 <kchinnamasthanbabu@gmail.com>
2025-10-01 03:02:21 +0530

    Added start.sh and stop.sh for CodeDeploy

---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup java -jar /home/ec2-user/app/springboot-restful-webservices-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
---
+++ scripts/stop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pkill -f springboot-restful-webservices-0.0.1-SNAPSHOT.jar
